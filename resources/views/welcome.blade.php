23846ed102bb80767d581f563cdd9082e27811eb
Bernard <bchisumo74@gmail.com>
2018-11-08 14:22:44 +0000

    Databases and Migrations

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,8 @@
 @extends('layout.master')
 @section('title', ' Home page')
 @section('main-content')
-    <h1>My {{ $foo }}First website</h1>
+    <h1>MyFirst website</h1>
 
-    @foreach($tasks as $task)
 
-       <li>{{ $task }}</li>
-
-    @endforeach
 
 @endsection
